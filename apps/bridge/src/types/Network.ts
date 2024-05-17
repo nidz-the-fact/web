03cb5e71080831e48e83ecf8d811e3fe6fb1afba
Nidz The Fact <106298826+nidz-the-fact@users.noreply.github.com>
2024-05-17 15:00:05 +0700

    Update Network.ts

--- apps/bridge/src/types/Network.ts
+++ apps/bridge/src/types/Network.ts
@@ -1,1 +1,1 @@
-export type Network = 'homestead' | 'goerli' | 'sepolia' | 'base' | 'base-goerli' | 'base-sepolia';
+export type Network = 'homestead' | 'goerli' | 'sepolia' | 'base' | 'base-goerli' | 'base-sepolia' | 'op-sepolia' | 'jbc' | 'hera-testnet';
